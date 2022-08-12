109db46b04b7f71384f065ab1e17a5e5092692ad
VERISBABY THẾ NHÂN <95742474+VERISBABY@users.noreply.github.com>
2022-08-12 23:02:29 +0700

    Add files via upload

---
+++ javascript/function.js
@@ -0,0 +1,35 @@
+// Nút đăng kí
+const button = document.querySelector(".dangkinews");
+
+function logSubmit(event) {
+    log.textContent = `SUBCRIBED SUCCESS!`;
+    event.preventDefault();
+  }
+  
+  const form = document.getElementById('form');
+  const log = document.getElementById('log');
+  form.addEventListener('submit', logSubmit);
+
+  // INDEX - SLIDER
+  var options = {
+  accessibility: true,
+  prevNextButtons: true,
+  arrowShape: {
+    x0: 10,
+    x1: 60,
+    y1: 40,
+    x2: 60,
+    y2: 35,
+    x3: 15
+  }
+};
+var slides = document.getElementsByClassName('carousel-cell');
+var flkty = new Flickity(carousel, options);
+
+flkty.on('scroll', function () {
+  flkty.slides.forEach(function (slide, i) {
+    var image = slides[i];
+    var x = (slide.target + flkty.x) * -1/3;
+    image.style.backgroundPosition = x + 'px';
+  });
+});
