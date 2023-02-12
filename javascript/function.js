cc8608407adcd580ffdcd4b11a2f75f70b335d7b
VERISBABY THẾ NHÂN <95742474+VERISBABY@users.noreply.github.com>
2023-02-12 10:04:49 +0700

    Update function.js
    Signed-off-by: VERISBABY THẾ NHÂN <95742474+VERISBABY@users.noreply.github.com>

--- javascript/function.js
+++ javascript/function.js
@@ -5,3 +5,26 @@ const hours = date.getHours();
 const minutes = date.getMinutes();
 const time = hours + ":" + (minutes < 10 ? "0" + minutes : minutes);
 document.getElementById("year").innerHTML = "VERISBABY " + year + " " + time;
+
+// DROPDOWN
+function headerNavFunction() {
+  var x = document.getElementsByClassName("header-nav-list")[0];
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+
+var dropdown = document.getElementsByClassName("dropdown")[0];
+dropdown.addEventListener("click", function() {
+  var dropdownContent = this.getElementsByClassName("dropdown-content")[0];
+  var plusMinus = this.getElementsByClassName("plus-minus")[0];
+  if (dropdownContent.style.display === "block") {
+    dropdownContent.style.display = "none";
+    plusMinus.innerHTML = "+";
+  } else {
+    dropdownContent.style.display = "block";
+    plusMinus.innerHTML = "-";
+  }
+});
