f6eae8265a54e384da787afed0e3b911465fd539
VERISBABY THẾ NHÂN <95742474+VERISBABY@users.noreply.github.com>
2023-02-12 12:51:58 +0700

    Update function.js
    Signed-off-by: VERISBABY THẾ NHÂN <95742474+VERISBABY@users.noreply.github.com>

--- javascript/function.js
+++ javascript/function.js
@@ -1,11 +1,6 @@
 // NGÀY GIỜ
+   document.getElementById("year").innerHTML = new Date().getFullYear() + "&copyVERISBABY ";
 
-const date = new Date();
-const year = date.getFullYear();
-const hours = date.getHours();
-const minutes = date.getMinutes();
-const time = hours + ":" + (minutes < 10 ? "0" + minutes : minutes);
-document.getElementById("year") = year +  "&copyVERISBABY "+" | " + time;  
   
 
 // DROPDOWN
