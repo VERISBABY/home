86e5e332b492cec8227ff1621b1141a7b115b361
VERISBABY THẾ NHÂN <95742474+VERISBABY@users.noreply.github.com>
2023-02-12 20:43:10 +0700

    Update function.js
    Signed-off-by: VERISBABY THẾ NHÂN <95742474+VERISBABY@users.noreply.github.com>

--- javascript/function.js
+++ javascript/function.js
@@ -4,19 +4,5 @@
   
 
 // DROPDOWN
-const dropdownToggle = document.getElementById("dropdown-toggle");
-const dropdownContent = document.getElementById("dropdown-content");
-const icon = document.getElementById("icon");
 
-dropdownToggle.addEventListener("click", function() {
-  if (dropdownContent.style.display === "show") {
-    dropdownContent.style.display = "none";
-    icon.classList.remove("fa-minus");
-    icon.classList.add("fa-plus");
-  } else {
-    dropdownContent.style.display = "block";
-    icon.classList.remove("fa-plus");
-    icon.classList.add("fa-minus");
-  }
-});
 
