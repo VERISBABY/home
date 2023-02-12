f4514e2d202c0fa4670a6d0daf46f4791e944e15
VERISBABY THẾ NHÂN <95742474+VERISBABY@users.noreply.github.com>
2023-02-12 09:59:16 +0700

    Update function.js
    Signed-off-by: VERISBABY THẾ NHÂN <95742474+VERISBABY@users.noreply.github.com>

--- javascript/function.js
+++ javascript/function.js
@@ -1,37 +1,7 @@
-// Nút đăng kí
-const button = document.querySelector(".dangkinews");
-
-function logSubmit(event) {
-    log.textContent = `SUBCRIBED SUCCESS!`;
-    event.preventDefault();
-  }
-  
-  const form = document.getElementById('form');
-  const log = document.getElementById('log');
-  form.addEventListener('submit', logSubmit);
-
-  // INDEX - SLIDER
-  var options = {
-  accessibility: true,
-  prevNextButtons: true,
-  arrowShape: {
-    x0: 10,
-    x1: 60,
-    y1: 40,
-    x2: 60,
-    y2: 35,
-    x3: 15
-  }
-};
-var slides = document.getElementsByClassName('carousel-cell');
-var flkty = new Flickity(carousel, options);
-
-flkty.on('scroll', function () {
-  flkty.slides.forEach(function (slide, i) {
-    var image = slides[i];
-    var x = (slide.target + flkty.x) * -1/3;
-    image.style.backgroundPosition = x + 'px';
-  });
-});
-
-document.getElementById("year").innerHTML = "&copyVERISBABY " + new Date().getFullYear();
+// NGÀY GIỜ
+const date = new Date();
+const year = date.getFullYear();
+const hours = date.getHours();
+const minutes = date.getMinutes();
+const time = hours + ":" + (minutes < 10 ? "0" + minutes : minutes);
+document.getElementById("date").innerHTML = "VERISBABY " + year + " " + time;
