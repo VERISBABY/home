1591c115c39b7f2b353456206234acd7a1fdc0c1
VERISBABY THẾ NHÂN <95742474+VERISBABY@users.noreply.github.com>
2023-03-21 19:21:32 +0700

    Update function.js
    Signed-off-by: VERISBABY THẾ NHÂN <95742474+VERISBABY@users.noreply.github.com>

--- javascript/function.js
+++ javascript/function.js
@@ -75,3 +75,17 @@ var textWrapperTimeline = anime.timeline({loop: true})
     easing: "easeOutExpo",
     delay: 1000
   });
+
+window.addEventListener("load", function(){
+    setTimeout(
+        function open(event){
+            document.querySelector(".popup").style.display = "block";
+        },
+        2000 
+    )
+});
+
+
+document.querySelector("#close").addEventListener("click", function(){
+    document.querySelector(".popup").style.display = "none";
+});
